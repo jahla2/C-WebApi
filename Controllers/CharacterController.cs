f7560e37396fee4b612cd4599708ae4dd1b708ba
jahla2 <rocso58@gmail.com>
2023-03-20 13:09:40 +0800

    Adding new Character using POST

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -8,22 +8,35 @@ namespace Dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
+        //Method 
         private static List<Character> characters = new List<Character>{
             new Character(),
             new Character { id = 1, name = "Sam"}
         };
-        //Route for GetAll records
+        //Controller with Route for GetAll records
         [HttpGet("GetAll")]
         public ActionResult<List<Character>> Get()
         {
             return Ok(characters);
         }
 
-        //Routing with parameters
+        //Controller with Routing with parameters  
+        //Send id Via URL
         [HttpGet("{id}")]
         public ActionResult<Character> GetSingle(int id)
         {
             return Ok(characters.FirstOrDefault(c => c.id == id));
+        }
+
+        //method for Adding new Record
+        //Send via JSONObject
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newChracter)
+        {
+            //Add new
+            characters.Add(newChracter);
+            //Retrun List
+            return Ok(characters);
         }
     }
 }
